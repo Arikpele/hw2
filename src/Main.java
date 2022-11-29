ab3869d822ce05eb0dbb81348b8f0ab158961519
Arikpele <79112835522@y.ru>
2022-11-29 20:27:48 +0300

    обновление

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,6 @@ public class Main {
         var cat = 3.5;
         var paper = 763789;
         var mouse = dog+4;
-        System.out.println(mouse);
-        var man=cat+4;
-        System.out.println(man);
-        var woman=paper+4;
-        System.out.println(woman);
+
     }
 }
