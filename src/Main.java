e292546cda6432e94550958d6f4ec6c8f8f3fdfd
Arikpele <79112835522@y.ru>
2022-11-29 20:54:28 +0300

    домашка 6

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        var frog=3.5;
-        System.out.println(frog);
-            frog=frog*10;
-        System.out.println(frog);
-        frog=frog/3.5;
-        System.out.println(frog);
-            frog=frog+4;
-        System.out.println(frog);
+      var man=78.2;
+              var woman=82.7;
+        System.out.println(man+woman);
     }
 }
