fec3d18366c8066d49ea97cf0338585918d129b8
Arikpele <79112835522@y.ru>
2022-11-29 22:54:56 +0300

    домашка 1 (редактирование)

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 10;
-        var cat = 3.5;
+        var dog = 8;
+        var cat = 3.6;
         var paper = 763789;
-        var mouse = dog+4;
-
+        System.out.println (dog);
+        System.out.println(cat);
+        System.out.println(paper);
     }
 }
