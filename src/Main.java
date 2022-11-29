8fd215071f8112ba8db3bc6e2075a52c9a7e543b
Arikpele <79112835522@y.ru>
2022-11-29 21:29:37 +0300

    домашка 7

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,10 @@ public class Main {
     public static void main(String[] args) {
       var manCool=78.2;
       var womanCool=82.7;
-        System.out.println(manCool+womanCool);
-        System.out.println(womanCool-manCool);
+      var c=womanCool-manCool;
+      System.out.println("разница в весе"+c+"кг");
+      var over = (womanCool+manCool)%c;
+      System.out.println (over);
+
     }
 }
