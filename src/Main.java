6abef3f4b884e91f6bae7be24282650ea15db8d5
Arikpele <79112835522@y.ru>
2022-11-29 22:40:54 +0300

    домашка 7 (редактирование)

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-      var manCool=78.2;
-      var womanCool=82.7;
-      var c=womanCool-manCool;
-      System.out.println("разница в весе"+c+"кг");
-      var over = (womanCool+manCool)%c;
+      var firstBoxer=78.2;
+      var secondBoxer=82.7;
+      var weightDifference=secondBoxer-firstBoxer;
+      System.out.println("разница в весе"+weightDifference+"кг");
+      var over = (secondBoxer+firstBoxer)%weightDifference;
       System.out.println (over);
 
     }
