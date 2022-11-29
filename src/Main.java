5e488749e33627027c44afcc1e5a731a4da14b98
Arikpele <79112835522@y.ru>
2022-11-29 22:01:06 +0300

    домашка 8

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-      var manCool=78.2;
-      var womanCool=82.7;
-      var c=womanCool-manCool;
-      System.out.println("разница в весе"+c+"кг");
-      var over = (womanCool+manCool)%c;
-      System.out.println (over);
+      var time=640;
+      var Man=8;
+      var c=time/Man;
+      System.out.println("Всего работников в компании — "+c+"человек");
+      var d=94;
+      var r=c+d;
+      var t=time/r;
+      System.out.println("Если в компании работает" + r + "человек, то всего"+t+"часов работы может быть поделено между сотрудниками");
+
 
     }
 }
