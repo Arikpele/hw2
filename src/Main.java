f7961aec846fd8016784edf352d35540b517c7ae
Arikpele <79112835522@y.ru>
2022-11-29 20:09:37 +0300

    дз2

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 10;
+        var cat = 3.5;
+        var paper = 763789;
+        var mouse = dog+4;
+        System.out.println(mouse);
+        var man=cat+4;
+        System.out.println(man);
+        var woman=paper+4;
+        System.out.println(woman);
+    }
+}
